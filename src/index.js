e03262ada906965dd322eeb8c75e8949567e7239
JTris1 <johnathontrisler@gmail.com>
2022-04-27 15:24:22 -0400

    Added broken Add Item feature

--- src/index.js
+++ src/index.js
@@ -54,6 +54,51 @@ let instruments = [
     }
 ];
 
+let list = instruments.slice();
+let renderList = [];
+
+function CartItem(props) {
+    return ReactDOM.createPortal (
+        <div className="cart-item">
+            <div className="info">
+                <h3 className="name">{props.name}</h3>
+                <h4 className="brand">{props.brand}</h4>
+                <h4 className="price">${props.price}</h4>
+            </div>
+            <button onClick={() => props.toggleShow()}>Remove</button>
+        </div>,
+        document.getElementById('cart-items')
+    )
+}
+
+class PopupForm extends React.Component {
+    addItem(name, brand, price) {
+        this.props.toggleShow();
+
+        return ReactDOM.createPortal (
+            <Item name={name} brand={brand} price={price} />,
+            document.getElementById("cards-wrapper")
+        );
+    }
+
+    render() {
+        return ReactDOM.createPortal (
+            <>
+                <div className="popup-bg"></div>
+                <div className="popup">
+                    <div>
+                        <input type="text" name="name" placeholder="Name"></input>
+                        <input type="text" name="brand" placeholder="Brand"></input>
+                        <input type="text" name="price" placeholder="Price"></input>
+                        <button onClick={() => this.addItem()}>Add</button>
+                    </div>
+                </div>
+            </>,
+            document.getElementById('portal')
+        )
+    }
+}
+
 function Popup(props) {
     return ReactDOM.createPortal (
         <>
@@ -62,7 +107,7 @@ function Popup(props) {
                 <img src={props.imgSrc} alt="Instrument"></img>
                 <h1 className="name">{props.name}</h1>
                 <h2 className="brand">{props.brand}</h2>
-                <button>Add to Cart</button>
+                <button onClick={() => props.toggleShow()}>Add to Cart</button>
             </div>
         </>,
         document.getElementById('portal')
@@ -77,14 +122,9 @@ class AddToCart extends React.Component {
         };
     }
 
-    addToCart() {
-        this.props.toggleShow();
-        
-    }
-
     render() {
         return(
-            this.state.show ? <button onClick={() => this.addToCart()}>Add to Cart</button> : null
+            this.state.show ? <button onClick={() => this.props.toggleShow()}>Add to Cart</button> : null
         );
     }
 }
@@ -104,18 +144,41 @@ class Item extends React.Component {
         this.setState({
             show: !this.state.show
         });
+        
+        for(let i in list) {
+            if(list[i].name === this.props.name) {
+                list.splice(i, 1);
+                break;
+            }
+            else {
+                list.push({name: this.props.name, type: this.props.type, price: this.props.price, brand: this.props.brand, imgSrc: this.props.imgSrc, color: this.props.color});
+                break;
+            }
+        }
+    }
+
+    renderCartItem() {
+        return (
+            <CartItem name={this.props.name} brand={this.props.brand} price={this.props.price} toggleShow={this.toggleShow} />
+        )
     }
 
     renderAddToCart(key) {
         return (
-            <AddToCart key={parseInt(key)} toggleShow={this.toggleShow} />
+            <AddToCart key={parseInt(key)} name={this.props.name} brand={this.props.brand} price={this.props.price} toggleShow={this.toggleShow} />
+        )
+    }
+    
+    renderEmptyText() {
+        return (
+            <h1>There are no more items to display!</h1>
         )
     }
 
     renderPopup(imgSrc, name, brand, color) {
         return (
             <div>
-                <Popup imgSrc={this.props.imgSrc} name={this.props.name} brand={this.props.brand} color={this.props.color} />
+                <Popup imgSrc={this.props.imgSrc} name={this.props.name} brand={this.props.brand} color={this.props.color} toggleShow={this.toggleShow} />
             </div>
         ) 
     }
@@ -131,6 +194,7 @@ class Item extends React.Component {
                 </img>
                 <h1 className="name">{this.props.name}</h1>
                 <h2 className="brand">{this.props.brand}</h2>
+                <h2 className="price">${this.props.price}</h2>
                 {this.state.showPopup ? this.renderPopup() : null}
             </div>
         )
@@ -140,7 +204,9 @@ class Item extends React.Component {
         return (
             <>
                 {this.state.show ? this.renderItem() : null}
+                {list.length === 0 ? this.renderEmptyText() : null}
                 {this.state.show ? this.renderAddToCart(1) : null}
+                {!this.state.show ? this.renderCartItem() : null}
             </>
         )
     }
@@ -164,9 +230,9 @@ class Cards extends React.Component {
     //     console.log("toggleShow()")
     // }
 
-    createItem(key, imgSrc, name, brand, color) {
+    createItem(key, imgSrc, name, brand, price, color) {
         return(
-            <Item key={key} toggleShow={this.toggleShow} imgSrc={imgSrc} name={name} brand={brand} color={color}/>
+            <Item key={key} toggleShow={this.toggleShow} imgSrc={imgSrc} name={name} brand={brand} price={price} color={color}/>
         )
     }
 
@@ -177,18 +243,16 @@ class Cards extends React.Component {
     // }
 
     render() {
-        let list = instruments.slice();
-        let renderList = [];
         for (let i in list) {
             renderList.push(
                 <div className="card">
-                    {this.createItem((parseInt(i) + 1), list[i].imgSrc, list[i].name, list[i].brand, list[i].color)}
+                    {this.createItem((parseInt(i) + 1), list[i].imgSrc, list[i].name, list[i].brand, list[i].price, list[i].color)}
                     {/* {this.createAddToCart(parseInt(i) * -1)} */}
                 </div>)
         }
 
         return(
-            <div className="cards-wrapper">
+            <div id="cards-wrapper" className="cards-wrapper">
                 {renderList}
             </div>
         );
@@ -205,28 +269,33 @@ class Listings extends React.Component {
     }
 }
 
-class CartItem extends React.Component {
-    render() {
-        return ReactDOM.createPortal (
-            <div className="cart-item">
-                <div className="info">
-                    <h3 className="name">{this.props.name}</h3>
-                    <h4 className="brand">{this.props.brand}</h4>
-                    <h4 className="price">{this.props.price}</h4>
-                </div>
-                <button>Remove</button>
-            </div>,
-            document.getElementById("cart-items")
-        )
-    }
-}
-
 class Cart extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            showForm: false
+        }
+
+        this.toggleShow = this.toggleShow.bind(this);
+    }
+
+    toggleShow() {
+        this.setState({
+            showForm: !this.state.showForm
+        });
+    }
+
+    // renderForm() {
+    //     this.setState({showForm: !this.state.showForm});
+    // }
+
     render() {
         return (
             <div className="cart">
+                {this.state.showForm ? <PopupForm toggleShow={this.toggleShow} /> : null}
                 <div className="header">
                     <h1>Cart</h1>
+                    <button onClick={() => this.toggleShow()}>Add Item</button>
                 </div>
                 <div id="cart-items">
                     {/* ADDED THROUGH PORTAL */}
